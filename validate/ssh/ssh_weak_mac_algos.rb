2a8476ed995c94d6f5328d8d9f13d02a273e16ea
Brandon McCann <mccann.brandon@gmail.com>
2014-04-23 06:56:06 -0500

    added ssh_weak_mac_algos.rb to enum weak ssh algos

---
+++ validate/ssh/ssh_weak_mac_algos.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+#
+# ssh_weak_mac_algos.rb
+# --------------------------------
+# Author: Brandon <zeknox> McCann
+# --------------------------------
+#
+# enumerate the list of supported ssh algos
+# from a list of ip addresses
+
+# ensure argument has been passed
+unless ARGV.length == 1
+  puts "\n\rUSAGE: ./ssh_weak_mac_algos.rb <ips.txt>\n\r"
+  exit!
+end
+
+@weak_algos = ['hmac-md5', 'hmac-md5-96', 'hmac-sha1-96']
+
+def parse_ssh_algos(output, ip=nil)
+  @weak_algos.each do |algo|
+    if output.include? algo
+      puts "[+] #{ip} - #{algo} Enabled"
+    end
+  end
+end
+
+# send dns_request
+def ssh_enumeration(ip)
+  puts "[*] Enumerating SSH algorithms against #{ip}"
+  nmap_output = `nmap -p 22 -sV #{ip} --script=ssh2-enum-algos`
+  parse_ssh_algos(nmap_output, ip)
+end
+
+# greet user
+puts "\n\r=== Testing for Weak SSH Algorithms ===\n\r"
+
+# read file and make dns request
+File.open(ARGV[0], "r") do |f|
+  f.each_line do |ip|
+    ssh_enumeration(ip.chomp)
+  end
+end
